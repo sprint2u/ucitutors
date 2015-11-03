9d948e2719474447d31a7feb6e68ab9bd3325978
Andy <yongwhoan@yahoo.com>
2015-11-03 12:24:34 -0800

    no message

--- k-12.php
+++ k-12.php
@@ -33,48 +33,49 @@
 </head>
 <body>
     <div id="wrapper">
-        <div class="header_container" >
-            <div class="header_block">
-                <header id="header" class="container clearfix" style="background-color:black; opacity:0.7;">
-                    <a class="logo" href="http://ucitutors.com/">UCI <span>Tutors</span></a>
-                    <nav class="menu_top_container">
-                        <ul class="menu_top">
-                            <li></li>
-                            <li><a href="">K-12 과외</a></li>
-                            <li><a href="">생활영어</a></li>
-                            <li><a href="">입학준비</a></li>
-                            <li><a href="">Tutor Profile</a></li>
-                            <li><a href="">Contact</a></li>
-                            <li></li>
-                        </ul>
-                    </nav>
-                </header>
-            </div>
-            <div class="page_title">
-                <div class="container">
-
-                <!-- BODY Starts -->
-                <h2 style="color: #85c04b;">K-12 과외</h2>
-                       
-                <table width="100%" style="background-color:white; opacity:0.8;">
-                <tr>
-                    <td width="100%">
-                        <h3 style="color:black;">Engish(Writing, Reading, Language Art)<br>
-                            Math, Science, Social Study 등<br>
-                            학교 교육과정을 도와주는 정통 학과 과외 입니다.</h3>
-                        <p></p>
-                    </td>
-                </tr>
-                </table>
-                <!-- BODY Ends -->
-
-                </div>
+        <div class="header" >
+            <div class="header_block" style="background-color:black; opacity:0.7;">
+               
+                <a class="logo" href="http://ucitutors.com/">UCI <span>Tutors</span></a>
+                <nav class="menu_top_container">
+                    <ul class="menu_top">
+                        <li></li>
+                        <li><a href="">K-12 과외</a></li>
+                        <li><a href="">생활영어</a></li>
+                        <li><a href="">입학준비</a></li>
+                        <li><a href="">Tutor Profile</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li></li>
+                    </ul>
+                </nav>
             </div>
         </div>
-        <footer style="background-color: black;">
+            <!-- BODY Starts -->
+
+        <div class="main">
+            <div class="container">
+
+            <h2 style="color: #85c04b;">K-12 과외</h2>
+                   
+            <table width="100%" style="background-color:white; opacity:0.8;">
+            <tr>
+                <td width="100%">
+                    <h3 style="color:black;">Engish(Writing, Reading, Language Art)<br>
+                        Math, Science, Social Study 등<br>
+                        학교 교육과정을 도와주는 정통 학과 과외 입니다.</h3>
+                    <p></p>
+                </td>
+            </tr>
+            </table>
+            </div>
+        </div>
+            <!-- BODY Ends -->
+
+        <div class="footer">
             <p class="copyright">Copyright © 2015 UCI Tutors</p>
             <p class="credit_footer"></p>
-        </footer>
+        </div>
+        
     </div>  
        
 </center>
