cc3fff2600848337981bb71ba10dc5d2f97eb302
Andy <yongwhoan@yahoo.com>
2015-11-05 15:45:49 -0800

    no message

--- k-12.php
+++ k-12.php
@@ -1,71 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>UCI Tutors :: 고품격 과외 네트워크</title>
-
-    <link rel="stylesheet" type="text/css" media="all" href="./Style/tutors.css" />
-    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css' />
-
-</head>
-<body>
-    <center>
-    <div id="wrapper">
-        <header class="header">
-        <table width="100%">
-        <tr height="70">
-            <td width="200" align="center"><a class="logo" href="http://ucitutors.com/">UCI <span>Tutors</span></a></td>
-            <td width="100"></td>
-            <td width="100" class="menu"><a href="k-12.php">K-12 과외</a></td>
-            <td width="100" class="menu"><a href="esl.php">생활영어</a></td>
-            <td width="100" class="menu"><a href="eld.php">입학준비</a></td>
-            <td width="100" class="menu"><a href="profile.php">Tutor Profile</a></td>
-            <td width="100" class="menu"><a href="contact.php">Contact</a></td>
-        </tr>
-        </table>
-        </header>
-
-        <!-- Main Starts -->
-        <main class="main">
-        <table width="90%">
-        <tr height="40">
-            <td>
-                 <br><div style="color: #85c04b; font-size: 25px;">K-12 과외<div>
-                 <br>
-            </td>
-        </tr>
-        <tr>
-            <td width="100%">
-                <img src="/Image/k-12.jpg" style="float:right">
-                <h3 style="color:black;">Engish(Writing, Reading, Language Art)<br>
-                Math, Science, Social Study 등 K-12(초,중,고) 교육과정을 도와주는
-                <br><b>정통 학과 과외</b> 입니다.</h3><br>
-                <ul>
-                    <li>대상: K-12 학생 (유치원, 초/중/고교생)<br><br></li>
-                    <li>수업형태: 회당 1시간/과목 (주 1~2회)
-                        <div>&nbsp;&nbsp;&nbsp;* 필요한 과목별로 수강하실 수 있습니다.</div>
-                        <div>&nbsp;&nbsp;&nbsp;* 학과지도 이외에 전과목 숙제 돌봐주기도 인기입니다.</div><br>
-                    </li>
-                    <li>수업료: (자택) $30/시간, (UCI) $25/시간
-                        <div>&nbsp;&nbsp;&nbsp;* 자택: 튜터가 자택에 방문하여 가르칩니다.</div>
-                        <div>&nbsp;&nbsp;&nbsp;* UCI: 학생이 UCI로 오셔서 배웁니다.</div>
-                    </li>
-                </ul>
-            </td>
-        </tr>
-        </table>
-        </main>
-        <!-- Main Ends -->
-
-        <footer id="footer">
-            <p>Copyright © 2015 UCI Tutors</p>
-        </footer>
-
-        
-    </div>  
-    
-       
-    </center>
-            
-</body>
-</html>
+<?php include('header.php'); ?>
+<!-- Main Starts -->
+<main class="main">
+<table width="90%">
+<tr height="40">
+    <td>
+         <br><div style="color: #85c04b; font-size: 25px;">K-12 과외<div>
+         <br>
+    </td>
+</tr>
+<tr>
+    <td width="100%">
+        <img src="/Image/k-12.jpg" style="float:right">
+        <h3 style="color:black;">Engish(Writing, Reading, Language Art)<br>
+        Math, Science, Social Study 등 K-12(초,중,고) 교육과정을 도와주는
+        <br><b>정통 학과 과외</b> 입니다.</h3><br>
+        <ul>
+            <li>대상: K-12 학생 (유치원, 초/중/고교생)<br><br></li>
+            <li>수업형태: 회당 1시간/과목 (주 1~2회)
+                <div>&nbsp;&nbsp;&nbsp;* 필요한 과목별로 수강하실 수 있습니다.</div>
+                <div>&nbsp;&nbsp;&nbsp;* 학과지도 이외에 전과목 숙제 돌봐주기도 인기입니다.</div><br>
+            </li>
+            <li>수업료: (자택) $30/시간, (UCI) $25/시간
+                <div>&nbsp;&nbsp;&nbsp;* 자택: 튜터가 자택에 방문하여 가르칩니다.</div>
+                <div>&nbsp;&nbsp;&nbsp;* UCI: 학생이 UCI로 오셔서 배웁니다.</div>
+            </li>
+        </ul>
+    </td>
+</tr>
+</table>
+</main>
+<!-- Main Ends -->
+<?php include('footer.php'); ?>
