c518cbf6ed34703dc1f3e037f58f48559a7f5ea4
Andy <yongwhoan@yahoo.com>
2015-11-05 15:44:46 -0800

    no message

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer id="footer">
+            <p>Copyright © 2015 UCI Tutors</p>
+        </footer>
+
+        
+    </div>  
+    
+       
+    </center>
+            
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,82 +1,44 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>UCI Tutors :: 고품격 과외 네트워크</title>
-
-    <link rel="stylesheet" type="text/css" media="all" href="./Style/tutors.css" />
-    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css' />
-
-</head>
-<body>
-    <center>
-    <div id="wrapper">
-        <header class="header">
-        <table width="100%">
-        <tr height="70">
-            <td width="200" align="center"><a class="logo" href="http://ucitutors.com/">UCI <span>Tutors</span></a></td>
-            <td width="100"></td>
-            <td width="100" class="menu"><a href="k-12.php">K-12 과외</a></td>
-            <td width="100" class="menu"><a href="esl.php">생활영어</a></td>
-            <td width="100" class="menu"><a href="eld.php">입학준비</a></td>
-            <td width="100" class="menu"><a href="profile.php">Tutor Profile</a></td>
-            <td width="100" class="menu"><a href="contact.php">Contact</a></td>
-        </tr>
-        </table>
-        </header>
-
-        <!-- Main Starts -->
-        <main class="intro">
-        <table width="100%" cellspacing="10">
-            <tr height="300" valign="top">
-                <td colspan="3" align="center">
-                    <div class="logo">
-                    <p><h1><span>Irvine 명문 </span>UCI<span> 학생들의</p> 
-                    <p>고품격 </span>과외<span> 네트워크</span></p>
-                    </div>
-                </td>
-            </tr>
-            <tr height="150">
-                <td width="33%" class="intro_table">
-                    <h2 style="color: #85c04b; font-weight: bold;">K-12 과외</h2>
-                    <h3>
-                        우리 아이, 한국에서 처럼 잘 할 수 있을까?
-                        <br>새로운 환경에서 적응해야 할 자녀에게
-                        <br>힘을 보태주세요.
-                    </h3>
-                    <p><a class="btn btn-default" href="k-12.php" style="color: #85c04b;">Learn more &raquo;</a></p>
-                </td>
-                <td width="33%" class="intro_table">
-                    <h2 style="color: #85c04b; font-weight: bold;">생활 영어</h2>
-                    <h3>
-                        스타벅스에서 커피 한잔 주문하는 것도<br>
-                        한국과는 많이 다르죠?<br>
-                        생활 영어는 어학이 아닌 익숙함 입니다.<br>
-                    </h3>
-                    <p><a class="btn btn-default" href="esl.php" style="color: #85c04b;">Learn more &raquo;</a></p>
-                </td>
-                <td width="33%" class="intro_table">
-                    <h2 style="color: #85c04b; font-weight: bold;">입학준비</h2>
-                    <h3>
-                        시작이 반!
-                        <br>ELD Test로부터 시작될 아이의 첫 미국학교 생활,
-                        <br>UCI Tutors와 함께 하세요.<br></h3>
-                    <p><a class="btn btn-default" href="eld.php" style="color: #85c04b;">Learn more &raquo;</a></p>
-                </td>
-            </tr>
-        </table>
-        </main>
-        <!-- Main Ends -->
-
-        <footer id="footer">
-            <p>Copyright © 2015 UCI Tutors</p>
-        </footer>
-
-        
-    </div>  
-    
-       
-    </center>
-            
-</body>
-</html>
+<?php include('header.php'); ?>
+<!-- Main Starts -->
+<main class="intro">
+<table width="100%" cellspacing="10">
+    <tr height="300" valign="top">
+        <td colspan="3" align="center">
+            <div class="logo">
+            <p><h1><span>Irvine 명문 </span>UCI<span> 학생들의</p> 
+            <p>고품격 </span>과외<span> 네트워크</span></p>
+            </div>
+        </td>
+    </tr>
+    <tr height="150">
+        <td width="33%" class="intro_table">
+            <h2 style="color: #85c04b; font-weight: bold;">K-12 과외</h2>
+            <h3>
+                우리 아이, 한국에서 처럼 잘 할 수 있을까?
+                <br>새로운 환경에서 적응해야 할 자녀에게
+                <br>힘을 보태주세요.
+            </h3>
+            <p><a class="btn btn-default" href="k-12.php" style="color: #85c04b;">Learn more &raquo;</a></p>
+        </td>
+        <td width="33%" class="intro_table">
+            <h2 style="color: #85c04b; font-weight: bold;">생활 영어</h2>
+            <h3>
+                스타벅스에서 커피 한잔 주문하는 것도<br>
+                한국과는 많이 다르죠?<br>
+                생활 영어는 어학이 아닌 익숙함 입니다.<br>
+            </h3>
+            <p><a class="btn btn-default" href="esl.php" style="color: #85c04b;">Learn more &raquo;</a></p>
+        </td>
+        <td width="33%" class="intro_table">
+            <h2 style="color: #85c04b; font-weight: bold;">입학준비</h2>
+            <h3>
+                시작이 반!
+                <br>ELD Test로부터 시작될 아이의 첫 미국학교 생활,
+                <br>UCI Tutors와 함께 하세요.<br></h3>
+            <p><a class="btn btn-default" href="eld.php" style="color: #85c04b;">Learn more &raquo;</a></p>
+        </td>
+    </tr>
+</table>
+</main>
+<!-- Main Ends -->    
+<?php include('footer.php'); ?>    
