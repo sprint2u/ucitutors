dae29f2d0347d665c8c21060ba1cc084461ef5a8
Andy <yongwhoan@yahoo.com>
2015-11-05 15:56:13 -0800

    no message

--- esl.php
+++ esl.php
@@ -12,11 +12,12 @@
     <td width="100%">
         <img src="/Image/esl.jpg" style="float:right">
         <h3 style="color:black;">레스토랑, 은행, 주유소, 마트, 은행 그리고 DMV 까지<br>
-        미국에서 살면서 겪게 될 상황들에 미리 대비할 수 있도록 도와드립니다.<br>
+        미국에 살면서 겪게 될 상황들을 미리 준비하세요.<br>
+        UCI Tutors와 함께라면 금새 Drive Thru에 들어갈 수 있습니다.<br>
         </h3><br>
         <ul>
-            <li>대상: 성인, 학생<br><br></li>
-            <li>수업형태: 회당 1시간 (주 1~2회)
+            <li>대상: 성인 및 학생<br><br></li>
+            <li>수업형태: 회당 1시간 (주 1~2회)<br>
                 <br>
             </li>
             <li>수업료: (자택) $30/시간, (UCI) $25/시간
