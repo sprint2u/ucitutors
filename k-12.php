e6e5ac0d7ff488dca519e3b2af25f2e53a79977f
Andy <yongwhoan@yahoo.com>
2015-11-04 22:27:50 -0800

    no message

--- k-12.php
+++ k-12.php
@@ -51,23 +51,23 @@
         <!-- Main Starts -->
 
         <main>
-            <div class="container">
-                <table width="100%" style="background-color:white; opacity:0.8;">
-                <tr>
-                    <td>
-                        <h2 style="color: #85c04b;">K-12 과외</h2>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="100%">
-                        <h3 style="color:black;">Engish(Writing, Reading, Language Art)<br>
-                            Math, Science, Social Study 등<br>
-                            학교 교육과정을 도와주는 정통 학과 과외 입니다.</h3>
-                        <p></p>
-                    </td>
-                </tr>
-                </table>
-            </div>
+        <div class="main">
+            <table width="100%" style="background-color:white; opacity:0.8;">
+            <tr>
+                <td>
+                    <h2 style="color: #85c04b;">K-12 과외</h2>
+                </td>
+            </tr>
+            <tr>
+                <td width="100%">
+                    <h3 style="color:black;">Engish(Writing, Reading, Language Art)<br>
+                        Math, Science, Social Study 등<br>
+                        학교 교육과정을 도와주는 정통 학과 과외 입니다.</h3>
+                    <p></p>
+                </td>
+            </tr>
+            </table>
+        </div>
         </main>
         <!-- Main Ends -->
 
