0f278c630410e62c1005e09fbd6058e26d922b5b
Andy <yongwhoan@yahoo.com>
2015-11-05 15:22:12 -0800

    no message

--- index.php
+++ index.php
@@ -39,21 +39,25 @@
             <tr height="150">
                 <td width="33%" class="intro_table">
                     <h2 style="color: #85c04b; font-weight: bold;">K-12 과외</h2>
-                    <h3>우리 아이, 한국에서 처럼 잘 할 수 있을까?<br>새로운 환경에서 적응해야 할 자녀에게 힘을 보태주세요.</h3>
+                    <h3>우리 아이, 한국에서 처럼 잘 할 수 있을까?
+                        <br>새로운 환경에서 적응해야 할 자녀에게 힘을 보태주세요.</h3>
                     <p><a class="btn btn-default" href="k-12.php" style="color: #85c04b;">Learn more &raquo;</a></p>
                 </td>
                 <td width="33%" class="intro_table">
                     <h2 style="color: #85c04b; font-weight: bold;">생활 영어</h2>
                     <h3>
-                        스타벅스에서 커피 한잔 주문하는 것도 한국과는 많이 다르죠?<br>생활 영어는 어학이 아닌 익숙함 입니다.<br>
+                        스타벅스에서 커피 한잔 주문하는 것도<br>
+                        한국과는 많이 다르죠?<br>
+                        생활 영어는 어학이 아닌 익숙함 입니다.<br>
                     </h3>
                     <p><a class="btn btn-default" href="esl.php" style="color: #85c04b;">Learn more &raquo;</a></p>
                 </td>
                 <td width="33%" class="intro_table">
-                    <h2 style="color: #85c04b; font-weight: bold;">ELD Test 대비</h2>
+                    <h2 style="color: #85c04b; font-weight: bold;">입학준비</h2>
                     <h3>
                         시작이 반!
-                        <br>미국학교에서의 첫 걸음을 UCI Tutors와 함께 하세요.<br></h3>
+                        <br>ELD Test로부터 시작될 아이의 첫 미국학교 생활,
+                        <br>UCI Tutors와 함께 하세요.<br></h3>
                     <p><a class="btn btn-default" href="eld.php" style="color: #85c04b;">Learn more &raquo;</a></p>
                 </td>
             </tr>
