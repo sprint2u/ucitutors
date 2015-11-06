e9659cb6a59189b4175b2b6e52738339325380a0
Andy <yongwhoan@yahoo.com>
2015-11-05 16:35:18 -0800

    no message

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php include('header.php'); ?>
+<!-- Main Starts -->
+<main class="main">
+<table width="90%">
+<tr height="40">
+    <td>
+         <br><div style="color: #85c04b; font-size: 25px;">Contact<div>
+         <br>
+    </td>
+</tr>
+<tr>
+    <td width="100%">
+        <img src="/Image/k-12.jpg" style="float:right">
+        <h3 style="color:black;">튜터링 문의 및 신청<br>
+        </h3><br>
+        <ul>
+            <li>Nomi Lee
+                <div>&nbsp;&nbsp;&nbsp;* nomil@uci.edu</div>
+                <div>&nbsp;&nbsp;&nbsp;* (949)526-9788</div><br>
+            </li>
+            <li>Yong Kim
+                <div>&nbsp;&nbsp;&nbsp;* ucitutors@yahoo.com</div>
+                <div>&nbsp;&nbsp;&nbsp;* (949)344-3490</div>
+            </li>
+        </ul>
+    </td>
+</tr>
+</table>
+</main>
+<!-- Main Ends -->
+<?php include('footer.php'); ?>
