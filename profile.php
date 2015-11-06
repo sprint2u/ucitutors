ba234b64fb899f0c7a99dc5c1424518df7a63593
Andy <yongwhoan@yahoo.com>
2015-11-05 16:37:39 -0800

    no message

---
+++ profile.php
@@ -0,0 +1,33 @@
+<?php include('header.php'); ?>
+<!-- Main Starts -->
+<main class="main">
+<table width="90%">
+<tr height="40">
+    <td>
+         <br><div style="color: #85c04b; font-size: 25px;">Tutors' Profile<div>
+         <br>
+    </td>
+</tr>
+<tr>
+    <td width="100%">
+        <img src="/Image/esl.jpg" style="float:right">
+        <h3 style="color:black;">레스토랑, 은행, 주유소, 마트, 은행 그리고 DMV 까지<br>
+        미국에 살면서 겪게 될 상황들을 미리 준비하세요.<br>
+        UCI Tutors와 함께라면 금새 Drive Thru에 들어갈 수 있습니다.<br>
+        </h3><br>
+        <ul>
+            <li>대상: 성인 및 학생<br><br></li>
+            <li>수업형태: 회당 1시간 (주 1~2회)<br>
+                <br>
+            </li>
+            <li>수업료: (자택) $30/시간, (UCI) $25/시간
+                <div>&nbsp;&nbsp;&nbsp;* 자택: 튜터가 자택에 방문하여 가르칩니다.</div>
+                <div>&nbsp;&nbsp;&nbsp;* UCI: 학생이 UCI로 오셔서 배웁니다.</div>
+            </li>
+        </ul>
+    </td>
+</tr>
+</table>
+</main>
+<!-- Main Ends -->
+<?php include('footer.php'); ?>
