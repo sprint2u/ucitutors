81c51f528a9c740e0b36bc71bffb2dcec3cea455
Andy <yongwhoan@yahoo.com>
2015-11-06 19:42:53 -0800

    no message

--- profile.php
+++ profile.php
@@ -13,27 +13,40 @@
         <img src="/Image/songi.jpg" width="200">
         <h3 style="color:black;">Song Choi<br>
         </h3><br>
+        <p>전공: Pharmaceutical Sciences</p>
+        <p>과외경력: In summer of 2014, I worked at Manchester Academy and taught private lessons in English and Math to Kindergarten, Elementary, and Middle School students. I also volunteered at Good Samaritan Hospital for 4 years and was president of the Youth Drug Abuse Prevention Program.
+        </p>
         
     </td>
 	<td width="200">
         <img src="/Image/erick.jpg" width="200">
-        <h3 style="color:black;">Mark Kim<br>
+        <h3 style="color:black;">Erick Contreras<br>
         </h3><br>
+        <p>전공: Mathematics</p>
+        <p>
+        과외경력: Last two summers I tutored high schoolers in math through the non-profit company of NCRF. I have also tutored peers while in high school, and have helped my younger brother with his schoolwork as well.
+        </p>
         
     </td>
     <td width="200">
         <img src="/Image/mark.jpg" width="200">
         <h3 style="color:black;">Mark Kim<br>
         </h3><br>
-        
+        <p>전공: Psychology and Social Behavior</p>
+        <p>
+        과외경력: I volunteered at my middle school’s daycare throughout high school. At the daycare, we helped elementary school students with their studies and also played with them outside until their parents came to pick them up. When I am home, I help my younger sister with her homework when she needs assistance. In the summer of 2013, I volunteered at Cedar Sinai Hospital in the Rehabilitation Unit.
+        </p>        
     </td>  
 </tr>
 <tr>
     <td width="200">
         <img src="/Image/daniel.jpg" width="200">
-        <h3 style="color:black;">Song Choi<br>
+        <h3 style="color:black;">Daniel Chung<br>
         </h3><br>
-        
+        <p>전공: Computer Science</p>
+        <p>
+        과외경력: I have experience in tutoring, baby sitting, and as a summer fun junior leader. My responsibilities as a junior leader included assisting the summer fun leader in watching over the group of children(aprox. 20-30). I have dealt with children in small groups, big groups as well as one on one and in any age group.
+        </p>        
     </td>
     <td width="200">
         
