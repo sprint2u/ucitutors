9b2a3481e6a90a1a4f5929750cdb547e4d5a9a7a
Andy <yongwhoan@yahoo.com>
2015-11-10 10:07:03 -0800

    no message

--- tutor_registration.php
+++ tutor_registration.php
@@ -10,23 +10,23 @@
     </td>
 </tr>
 <tr>
-    <td width="150" align="right">Name</td>
+    <td width="250" align="right">Name</td>
     <td width="250"><input type="text" name="tutor_name" size="40" maxlength="100"></td>
     <td></td>
 </tr>
 <tr>
-    <td width="150" align="right">UCI Student ID</td>
+    <td width="250" align="right">UCI Student ID</td>
     <td><input type="text" name="uci_id" size="40" maxlength="10"></td>
     <td>(e.g. 98765432)</td>
 </tr>
 <tr>
-    <td width="150" align="right">Major</td>
+    <td width="250" align="right">Major</td>
     <td><input type="text" name="uci_major" size="40" maxlength="100"></td>
     <td>(e.g. Computer Engineering)</td>
 </tr>
 
 <tr>
-    <td width="150" align="right">Subject of Tutoring</td>
+    <td align="right">Subject of Tutoring</td>
     <td>
     	<select name="tutoring_subject" size="5" multiple>
     		<optgroup label="US Courses">
@@ -47,7 +47,7 @@
     <td>(Press shift and click for multiple choice)</td>
 </tr>
 <tr>
-    <td width="150" align="right">Tutoring Grade</td>
+    <td align="right">Tutoring Grade</td>
     <td>
     	<select name="tutoring_grade" size="3" multiple>
     		<option value="elementary">Elementary School</option>
@@ -62,17 +62,17 @@
     </td>
 </tr>
 <tr>
-    <td width="150" align="right">Korean School Name</td>
+    <td align="right">Korean School Name</td>
     <td><input type="text" name="kor_univ_name" size="30" maxlength="100"></td>
     <td>(e.g. 연세대학교)</td>
 </tr>
 <tr>
-    <td width="150" align="right">Korean School Major</td>
+    <td align="right">Korean School Major</td>
     <td><input type="text" name="kor_major" size="40" maxlength="100"></td>
     <td>(e.g. 경영학)</td>
 </tr>
 <tr>
-    <td width="150" align="right">Korean School Student ID</td>
+    <td align="right">Korean School Student ID</td>
     <td><input type="text" name="kor_id" size="20" maxlength="10"></td>
     <td>(e.g. 21342024)</td>
 </tr>
