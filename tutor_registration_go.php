0f46e80ce4fcad4cf04c892d47b920cc732f3ea4
Andy <yongwhoan@yahoo.com>
2015-11-12 14:12:49 -0800

    no message

--- tutor_registration_go.php
+++ tutor_registration_go.php
@@ -1,8 +1,8 @@
 <?php
 echo $_POST['tutor_name'];
-/*
+
 $tutor_name = addslashes($_POST('tutor_name');
-  echo "$tutor_name";
+  echo $tutor_name;
 
 $uci_id = addslashes($_POST('uci_id');
 $uci_major = addslashes($_POST('uci_major');
@@ -17,12 +17,12 @@ $kr_id_card = addslashes($_POST('kr_id_card');
 // 업로드한 파일이 저장될 디렉토리 정의
 $target_dir = "pds";  // 서버에 up 이라는 디렉토리가 있어야 한다.
 
-echo "$tutoring_subject";
+echo $tutoring_subject;
 $sql = "insert into tutor_profile
         values('$tutor_name','$uci_id','$uci_major','$tutoring_subject','$tutoring_grade',
         '$uci_id_card','$kr_univ_name',$kr_major,'$kr_id',$kr_id_card)";
 
-echo "$sql";
+echo $sql;
 /*
 if(strcmp($uci_id_card,"none")) {   // 파일이 업로드되었을 경우
 
