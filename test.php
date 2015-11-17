5f13d3d477c8d71ab7acb2d5738f54b3015f93f3
Andy <yongwhoan@yahoo.com>
2015-11-17 10:30:58 -0800

    no message

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+if ($_POST) {
+    echo '<pre>';
+    echo htmlspecialchars(print_r($_POST, true));
+    echo '</pre>';
+}
+?>
+<form action="" method="post">
+    이름: <input type="text" name="personal[name]" /><br />
+    메일: <input type="text" name="personal[email]" /><br />
+    맥주: <br />
+    <select multiple name="beer[]">
+        <option value="warthog">Warthog</option>
+        <option value="guinness">Guinness</option>
+        <option value="stuttgarter">Stuttgarter Schwabenbräu</option>
+    </select><br />
+    <input type="hidden" name="action" value="submitted" />
+    <input type="submit" value="전송합니다!" />
+</form>
