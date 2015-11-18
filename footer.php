f272aa4722a14669d8986d9c5933aa869674ead4
Andy <yongwhoan@yahoo.com>
2015-11-17 21:31:24 -0800

    no message

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
+		<table class="footer">
+	        <tr height="15">
+	            <td colspan="8" class="small_menu" align="right">
+	                <a href="tutor_registration.php">Tutor Registration</a>
+	                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	            </td>
+	        </tr>
+  	  	</table>		
 		<footer id="footer">
-            <p>Copyright © 2015 UCI Tutors</p>
+        <p>Copyright © 2015 UCI Tutors</p>
         </footer>
 
         
--- header.php
+++ header.php
@@ -27,11 +27,5 @@
             <td width="100" class="menu"><a href="profile.php">교사소개</a></td>
             <td width="100" class="menu"><a href="contact.php">Contact</a></td>
         </tr>
-        <tr height="15">
-            <td colspan="8" class="small_menu" align="right">
-                <a href="tutor_registration.php">Tutor Registration</a>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </td>
-        </tr>
         </table>
         </header>
