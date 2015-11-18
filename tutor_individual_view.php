9178cf5de4e19b50c3413a314265b85e2139b66d
Andy <yongwhoan@yahoo.com>
2015-11-17 20:33:26 -0800

    no message

--- tutor_individual_view.php
+++ tutor_individual_view.php
@@ -26,64 +26,64 @@ $result = mysql_query($query, $conn) or die (mysql_error());
     <td class="tbl_content"><?php echo $array[tutor_name]; ?></td>
   </tr>
   <tr>
-    <td>UCI Student #</td>
-    <td><?php echo $array[uci_id]; ?></td>
+    <td class="tbl_subject">UCI Student #</td>
+    <td class="tbl_content"><?php echo $array[uci_id]; ?></td>
   </tr>
   <tr>
-    <td>Major</td>
-    <td><?php echo $array[uci_major]; ?></td>
+    <td class="tbl_subject">Major</td>
+    <td class="tbl_content"><?php echo $array[uci_major]; ?></td>
   </tr>
   <tr>
-    <td>Mobile</td>
-    <td><?php echo $array[mobile]; ?></td>
+    <td class="tbl_subject">Mobile</td>
+    <td class="tbl_content"><?php echo $array[mobile]; ?></td>
   </tr>
   <tr>
-    <td>Email</td>
-    <td><?php echo $array[email]; ?></td>
+    <td class="tbl_subject">Email</td>
+    <td class="tbl_content"><?php echo $array[email]; ?></td>
   </tr>
   <tr>
-    <td>English</td>
-    <td><?php echo $array[english]; ?></td>
+    <td class="tbl_subject">English</td>
+    <td class="tbl_content"><?php echo $array[english]; ?></td>
   </tr>
   <tr>
-    <td>Korean</td>
-    <td><?php echo $array[korean]; ?></td>
+    <td class="tbl_subject">Korean</td>
+    <td class="tbl_content"><?php echo $array[korean]; ?></td>
   </tr>
   <tr>
-    <td>Tutoring Subjects</td>
-    <td><?php echo $array[tutoring_subject]; ?></td>
+    <td class="tbl_subject">Tutoring Subjects</td>
+    <td class="tbl_content"><?php echo $array[tutoring_subject]; ?></td>
   </tr>
   <tr>
-    <td>Tutoring Grades</td>
-    <td><?php echo $array[tutoring_grade]; ?></td>
+    <td class="tbl_subject">Tutoring Grades</td>
+    <td class="tbl_content"><?php echo $array[tutoring_grade]; ?></td>
   </tr>
   <tr>
-    <td>UCI Student Verification</td>
-    <td><?php echo "<img src=\"/pds/$array[uci_id_card]\" width=\"200\" />"; ?></td>
+    <td class="tbl_subject">UCI Student Verification</td>
+    <td class="tbl_content"><?php echo "<img src=\"/pds/$array[uci_id_card]\" width=\"200\" />"; ?></td>
   </tr>
   <tr>
-    <td>Tutor Photo</td>
-    <td><?php echo "<img src=\"/pds/$array[tutor_photo]\" width=\"200\" />"; ?></td>
+    <td class="tbl_subject">Tutor Photo</td>
+    <td class="tbl_content"><?php echo "<img src=\"/pds/$array[tutor_photo]\" width=\"200\" />"; ?></td>
   </tr>
   <tr>
-    <td>Tutor Video</td>
-    <td><?php echo "<img src=\"/pds/$array[tutor_video]\" width=\"200\" />"; ?></td>
+    <td class="tbl_subject">Tutor Video</td>
+    <td class="tbl_content"><?php echo "<img src=\"/pds/$array[tutor_video]\" width=\"200\" />"; ?></td>
   </tr>
   <tr>
-    <td>출신학교</td>
-    <td><?php echo $array[kr_univ_name]; ?></td>
+    <td class="tbl_subject">출신학교</td>
+    <td class="tbl_content"><?php echo $array[kr_univ_name]; ?></td>
   </tr>
   <tr>
-    <td>전공</td>
-    <td><?php echo $array[kr_major]; ?></td>
+    <td class="tbl_subject">전공</td>
+    <td class="tbl_content"><?php echo $array[kr_major]; ?></td>
   </tr>
   <tr>
-    <td>학번</td>
-    <td><?php echo $array[kr_id]; ?></td>
+    <td class="tbl_subject">학번</td>
+    <td class="tbl_content"><?php echo $array[kr_id]; ?></td>
   </tr>
   <tr>
-    <td>출신학교 확인</td>
-    <td><?php echo "<img src=\"/pds/$array[kr_id_card]\" width=\"200\" />"; ?></td>
+    <td class="tbl_subject">출신학교 확인</td>
+    <td class="tbl_content"><?php echo "<img src=\"/pds/$array[kr_id_card]\" width=\"200\" />"; ?></td>
   </tr>
 <?php
   }
