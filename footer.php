2373fe7211cf09c20be8e1cc295bce69f8d46b16
Andy <yongwhoan@yahoo.com>
2015-11-17 21:34:38 -0800

    no message

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 		<table class="footer">
-	        <tr height="10">
+	        <tr height="6">
 	            <td colspan="8" class="small_menu" align="right">
 	                <a href="tutor_registration.php">Tutor Registration</a>
 	                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
