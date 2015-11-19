e4d3628a6255b0ec16be22e06172d3fe8529bc8b
Andy <yongwhoan@yahoo.com>
2015-11-18 21:57:30 -0800

    no message

--- manager/header.php
+++ manager/header.php
@@ -9,7 +9,6 @@
 </head>
 <body>
     <center>
-        <header class="header">
         <table width="100%">
         <tr height="70">
             <td width="300" align="center"><a class="logo" href="http://ucitutors.com/">UCI <span>Tutors</span></a></td>
@@ -22,4 +21,3 @@
             <td width="100" class="menu"><a href="../contact.php">Contact</a></td>
         </tr>
         </table>
-        </header>
