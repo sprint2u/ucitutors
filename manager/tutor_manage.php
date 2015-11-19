0bea45afc27e9f6df488b88f347224494e7e6e09
Andy <yongwhoan@yahoo.com>
2015-11-18 22:02:21 -0800

    no message

--- manager/tutor_manage.php
+++ manager/tutor_manage.php
@@ -1,10 +1,9 @@
-<?php //include('header.php'); ?>
 <?php
 $tutor_name = addslashes($_POST['tutor_name']);
 $uci_id = addslashes($_POST['uci_id']);
 ?>
 <main class="main">
-<table width="100%">
+<table width="1600">
 <tr height="40">
     <td colspan="4">
          <br><div style="color: #85c04b; font-size: 25px;">Tutor Manager<div>
