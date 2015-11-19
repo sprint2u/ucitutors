937f5283ce0ded24266ffb5a1d076afbf509cf77
Andy <yongwhoan@yahoo.com>
2015-11-18 23:07:49 -0800

    no message

--- footer.php
+++ footer.php
@@ -1,14 +1,16 @@
 		<table class="footer">
 	        <tr height="15px">
+	        	<td align="left">
+	        		<footer id="footer">
+			        <p>Copyright © 2015 UCI Tutors</p>
+			        </footer>
+	        	</td>
 	            <td colspan="8" class="small_menu" align="right">
 	                <a href="tutor_registration.php">Tutor Application</a>
 	                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 	            </td>
 	        </tr>
   	  	</table>		
-		<footer id="footer">
-        <p>Copyright © 2015 UCI Tutors</p>
-        </footer>
 
         
     </div>  
