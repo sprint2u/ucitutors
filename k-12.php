c5064287408cb6719ae0485a9289e9076a162221
Andy <yongwhoan@yahoo.com>
2016-01-09 11:31:41 -0800

    no message

--- k-12.php
+++ k-12.php
@@ -20,7 +20,37 @@
                 <div>&nbsp;&nbsp;&nbsp;* 필요한 과목별로 수강하실 수 있습니다.</div>
                 <div>&nbsp;&nbsp;&nbsp;* 학과지도 이외에 전과목 숙제 돌봐주기도 인기입니다.</div><br>
             </li>
-            <li>수업료: (자택) $35/시간, (UCI) $25/시간
+            <li>수업료 (last update on 1/10/2016)
+                <table>
+                    <tr>
+                        <td></td>
+                        <td>K-6</td>
+                        <td>7-8</td>
+                        <td>9-12></td>
+                        <td>성인</td>
+                    </tr>
+                    <tr>
+                        <td>영어회화</td>
+                        <td>$30</td>
+                        <td>$30</td>
+                        <td>$30</td>
+                        <td>$30</td>
+                    </tr>
+                    <tr>
+                        <td>학과과외</td>
+                        <td>30</td>
+                        <td>35</td>
+                        <td>35</td>
+                        <td>N/A</td>
+                    </tr>
+                    <tr>
+                        <td>SKY</td>
+                        <td>40</td>
+                        <td>45</td>
+                        <td>50</td>
+                        <td>N/A</td>
+                    </tr>
+                </table>
                 <div>&nbsp;&nbsp;&nbsp;* 자택: 튜터가 자택에 방문하여 가르칩니다.</div>
                 <div>&nbsp;&nbsp;&nbsp;* UCI: 학생이 UCI로 오셔서 배웁니다.</div>
             </li>
