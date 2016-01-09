126a4319ace57c44c5a78056b3159619f0d2137b
Andy <yongwhoan@yahoo.com>
2016-01-09 12:37:57 -0800

    Price plan 캡슐화

--- k-12.php
+++ k-12.php
@@ -20,42 +20,7 @@
                 <div>&nbsp;&nbsp;&nbsp;* 필요한 과목별로 수강하실 수 있습니다.</div>
                 <div>&nbsp;&nbsp;&nbsp;* 학과지도 이외에 전과목 숙제 돌봐주기도 인기입니다.</div><br>
             </li>
-            <li>수업료 (last update on 1/10/2016)
-                <span>&nbsp;&nbsp;&nbsp;
-                    <table>
-                        <tr>
-                            <td width="100" class="price_head">분류</td>
-                            <td width="50" class="price_head">K-6</td>
-                            <td width="50" class="price_head">7-8</td>
-                            <td width="50" class="price_head">9-12</td>
-                            <td width="50" class="price_head">성인</td>
-                        </tr>
-                        <tr>
-                            <td class="price_body">영어회화</td>
-                            <td class="price_body">$30</td>
-                            <td class="price_body">$30</td>
-                            <td class="price_body">$30</td>
-                            <td class="price_body">$30</td>
-                        </tr>
-                        <tr>
-                            <td class="price_body">학과과외</td>
-                            <td class="price_body">$30</td>
-                            <td class="price_body">$35</td>
-                            <td class="price_body">$35</td>
-                            <td class="price_body">N/A</td>
-                        </tr>
-                        <tr>
-                            <td class="price_body">SKY</td>
-                            <td class="price_body">$40</td>
-                            <td class="price_body">$45</td>
-                            <td class="price_body">$50</td>
-                            <td class="price_body">N/A</td>
-                        </tr>
-                    </table>
-                </span>
-                <div>&nbsp;&nbsp;&nbsp;* 위 가격표는 학생이 UCI로 오셔서 배우는 경우에 적용됩니다.</div>
-                <div>&nbsp;&nbsp;&nbsp;* 자택 방문 과외 시 위 요금에서 $5 추가됩니다.</div>
-            </li><br>
+            <?php include('price_plan.php'); ?>
         </ul>
     </td>
 </tr>
