9343c11412d3c91e1187fe63c16241a99715a3c5
Andy <yongwhoan@yahoo.com>
2016-01-09 12:25:59 -0800

    Price plan update on 1/9/2016

--- sky.php
+++ sky.php
@@ -21,9 +21,41 @@
             <li>수업형태: 회당 1시간 (주 1~2회)<br>
                 <br>
             </li>
-            <li>수업료: (자택) $45/시간, (UCI) $35/시간
-                <div>&nbsp;&nbsp;&nbsp;* 자택: 튜터가 자택에 방문하여 가르칩니다.</div>
-                <div>&nbsp;&nbsp;&nbsp;* UCI: 학생이 UCI로 오셔서 배웁니다.</div>
+            <li>
+                <span>&nbsp;&nbsp;&nbsp;
+                    <table>
+                        <tr>
+                            <td width="100" class="price_head">분류</td>
+                            <td width="50" class="price_head">K-6</td>
+                            <td width="50" class="price_head">7-8</td>
+                            <td width="50" class="price_head">9-12</td>
+                            <td width="50" class="price_head">성인</td>
+                        </tr>
+                        <tr>
+                            <td class="price_body">영어회화</td>
+                            <td class="price_body">$30</td>
+                            <td class="price_body">$30</td>
+                            <td class="price_body">$30</td>
+                            <td class="price_body">$30</td>
+                        </tr>
+                        <tr>
+                            <td class="price_body">학과과외</td>
+                            <td class="price_body">$30</td>
+                            <td class="price_body">$35</td>
+                            <td class="price_body">$35</td>
+                            <td class="price_body">N/A</td>
+                        </tr>
+                        <tr>
+                            <td class="price_body">SKY</td>
+                            <td class="price_body">$40</td>
+                            <td class="price_body">$45</td>
+                            <td class="price_body">$50</td>
+                            <td class="price_body">N/A</td>
+                        </tr>
+                    </table>
+                </span>
+                <div>&nbsp;&nbsp;&nbsp;* 위 가격표는 학생이 UCI로 오셔서 배우는 경우에 적용됩니다.</div>
+                <div>&nbsp;&nbsp;&nbsp;* 자택 방문 과외 시 위 요금에서 $5 추가됩니다.</div>
             </li>
         </ul>
     </td>
